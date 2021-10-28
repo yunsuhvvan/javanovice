3816272d5ffcc22b667ce48450ad57ff0f2319d6
yunsuhvvan <87231093+yunsuhvvan@users.noreply.github.com>
2021-10-28 13:58:56 +0900

    Update FoodTest.java

--- PRACTICE/oop3/FoodTest.java
+++ PRACTICE/oop3/FoodTest.java
@@ -18,10 +18,32 @@ public class FoodTest {
 	  }
 	}
 
-	class Food {
-	  /* 1. 필드를 추가하세요. */
-	  
-	  /* 2. 생성자를 추가하세요. */
-	  
-	  /* 3. toString() 메소드를 추가하세요. */
+class Food {
+	/* 1. 필드를 추가하세요. */
+
+	String name;
+	int price;
+
+	/* 2. 생성자를 추가하세요. */
+
+	public Food(){
 	}
+	
+	public Food(String name, int price){
+		this.name = name;
+		this.price = price;
+	}
+
+	/* 3. toString() 메소드를 추가하세요. */
+	// 오버라이딩하기
+	public String toString(){   //최상위 object클래스가 가진 메소드 중 하나
+
+		return "name : " +this.name + "," + "price : " + this.price + "원";
+	}
+}  
+//실행결과
+//name : 치킨,price : 18000원
+//name : 피자,price : 28000원
+//name : 초밥세트,price : 22000원
+
+
